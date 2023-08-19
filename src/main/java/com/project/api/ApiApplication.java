75e5b02e9f2c6f4b98bf0b472fc199b7a1f8e0bc
João Cabral <joao.cabral@mobtelecom.com.br>
2023-08-18 22:12:00 -0300

    :sparkles: : H2 database, JPA

--- src/main/java/com/project/api/ApiApplication.java
+++ src/main/java/com/project/api/ApiApplication.java
@@ -2,12 +2,15 @@ package com.project.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(ApiApplication.class, args);
+		Object dataSource = context.getBean("dataSource");
+		System.out.println(dataSource);
 	}
 
 }
