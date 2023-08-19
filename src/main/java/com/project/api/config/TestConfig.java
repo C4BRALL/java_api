176e3f0bb993649fde0c26b5909032e8f20d9c31
João Cabral <joao.cabral@mobtelecom.com.br>
2023-08-19 18:11:19 -0300

    :sparkles: : JPA Repository, dependency injection, database seeding

---
+++ src/main/java/com/project/api/config/TestConfig.java
@@ -0,0 +1,29 @@
+package com.project.api.config;
+
+import com.project.api.entities.User;
+import com.project.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+
+  @Autowired
+  private UserRepository userRepository;
+
+
+  @Override
+  public void run(String... args) throws Exception {
+
+    User u1 = new User(1L, "Maria Brown", "maria@email.com", "85986198071", "passwordteste");
+    User u2 = new User(2L, "Alex Green", "alex@email.com", "85987208182", "passwordteste");
+
+    userRepository.saveAll(Arrays.asList(u1, u2));
+  }
+}
